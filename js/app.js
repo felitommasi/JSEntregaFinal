c108eb0ffe7d5297d1e15086e73f21c2a920104d
felitommasi <felipe.tommasi@gmail.com>
2022-05-11 20:02:53 -0300

    add bootstrap, sidebar

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//popper sidebar
+//inicializar popper segun documentacion
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
