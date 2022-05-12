84d1e8932b14cb61b3b5fbea3b5c8290497c251a
felitommasi <felipe.tommasi@gmail.com>
2022-05-12 20:11:17 -0300

    add nodes, event listeners

--- js/app.js
+++ js/app.js
@@ -19,27 +19,67 @@ const cargarData = async () => {
 };
 cargarData();
 
-
 //seleccionar nodos 
 const selectPartida = document.querySelector('#selectPartida');
 const selectDestino = document.querySelector('#selectDestino');
-
-
-//crear texto para <option></option> dentro de <select></select> 
-//textoOption se reemplaza con texto del JSON
+const form = document.querySelector('#formCompra');
+const btnCalcular = document.querySelector('#btnCalcular');
+const btnComprar = document.querySelector('#btnComprar');
+const btnCancelar = document.querySelector('#btnCancelar');
 
 
 //pintar data en options
 const pintarOptions = (arrayData) => {	
 	for (let i = 0; i < arrayData.length; i++) {
-    let option = document.createElement('option');
-    selectPartida.appendChild(option);
-		let textoOption = document.createTextNode(`${arrayData[i].estacion}`);
-    option.append(textoOption);
-	}
+    //crear nodos option
+    let optionPartida = document.createElement('option');
+    optionPartida.classList.add("partida");
+
+    //agregar nodos al select
+    selectPartida.appendChild(optionPartida);
+		
+    //traer texto de JSON para completar option's
+    let estacionesPartida = document.createTextNode(`${arrayData[i].estacion}`);
+    optionPartida.append(estacionesPartida);
+	} 
+
+  for (let i = 0; i < arrayData.length; i++) {
+    //crear nodos option
+    let optionDestino = document.createElement('option');
+    optionDestino.classList.add("destino");
+
+    //agregar nodos al select
+    selectDestino.appendChild(optionDestino);
+		
+    //traer texto de JSON para completar option's
+    let estacionesDestino = document.createTextNode(`${arrayData[i].estacion}`);
+    optionDestino.append(estacionesDestino);
+	} 
 }
 
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
 
+  //esperar a que cargue el DOM
+  const optionPartida = document.getElementsByTagName('option');
+  
+  //obtener datos ingresados en select's
+  for (let i = 0 ; i < optionPartida.length; i++) {
+    console.log(optionPartida.length)
+  }
+
+
+})
+
+
+
+/*
+let obtenerTextoPartida = optionPartida.addEventListener('click', () =>{
+    console.log(selectPartida.value);
+    console.log(selectPartida.textContent);
+})
+
+ */
 
 
